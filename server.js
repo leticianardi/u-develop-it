25b26c91c2852cf9bcb4f717617ebc2c2a15d59f
leticianardi <leticia.tnardi@gmail.com>
2021-12-13 11:23:35 -0800

    credentials to data base

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+ {
+   host: 'localhost',
+   // Your MySQL username,
+   user: 'root',
+   // Your MySQL password
+   password: 'abobora.BRANCA12',
+   database: 'election'
+ },
+ console.log('Connected to the election database.')
+);
 
 
 // BOTTOM FUNCTIONS
